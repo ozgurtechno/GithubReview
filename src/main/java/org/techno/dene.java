1aee80a68cf6e59e33ff7e05ae2be92fd939bf04
Ozgur Techno <ozgur.technostudy@gmail.com>
2022-11-25 18:45:02 +0300

    Den classi eklendi

--- src/main/java/org/techno/dene.java
+++ src/main/java/org/techno/dene.java
@@ -3,6 +3,6 @@ package org.techno;
 public class dene {
     public static void main(String[] args) {
         System.out.println("Yeni Eklendi 2");
-        System.out.println("Hello");
+        System.out.println("dnvkjsn");
     }
 }
